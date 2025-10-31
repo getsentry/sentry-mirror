7c44f51442cd3afd9d64afe6e2022f700a17ad73
Mark Story <mark@mark-story.com>
2025-10-31 15:32:00 -0400

    Fix config precedence

--- src/config.rs
+++ src/config.rs
@@ -99,14 +99,15 @@ impl Provider for ConfigData {
 pub fn from_args(args: &Args) -> Result<ConfigData, Box<figment::Error>> {
     let config_path = &args.config;
     let mut config: ConfigData = Figment::from(ConfigData::default())
-        .merge(Env::prefixed("SENTRY_MIRROR_"))
         .merge(Yaml::file(config_path))
+        .merge(Env::prefixed("SENTRY_MIRROR_"))
         .extract()?;
 
     if args.verbose {
         config.verbose = true;
         config.log_filter = Some("debug".into());
     }
+    dbg!(&config);
 
     Ok(config)
 }
